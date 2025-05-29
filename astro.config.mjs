cb5c4b28876bf53a81f296acb2ce28476618f785
edumoro412 <edumoro2003@gmail.com>
2025-05-29 21:47:02 +0200

    Update Astro configuration for Infiniti documentation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,63 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "INFINITI",
+      description: "Documentation for Infiniti project",
+      logo: {
+        src: "./public/infinito.png",
+        alt: "Logo de Infiniti",
+      },
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/edumoro412/infiniti",
+        },
+      ],
+      sidebar: [
+        {
+          label: "General",
+          items: [
+            {
+              label: "Introducción",
+              link: "/general/introduccion",
+            },
+            {
+              label: "Instalación",
+              link: "/general/instalacion",
+            },
+            {
+              label: "Estructura",
+              link: "/general/estructura",
+            },
+            {
+              label: "Configuración",
+              link: "/general/configuracion",
+            },
+            {
+              label: "Uso de la aplicación",
+              link: "/general/uso",
+            },
+          ],
+        },
+        {
+          label: "Desarrollo",
+          autogenerate: { directory: "desarrollo" },
+        },
+        {
+          label: "Componentes",
+          autogenerate: { directory: "componentes" },
+        },
+        {
+          label: "Problemas",
+          autogenerate: { directory: "problemas" },
+        },
+      ],
+    }),
+  ],
 });
